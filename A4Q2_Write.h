98765bb1f06133d857dff17869e23aa9c8f38506
sbroger <stefan.roger1@gmail.com>
2024-12-21 11:41:43 -0800

    Remove readme, remove comments, update for 2025 assignment version, clean up formatting, and unify project settings.

--- A4Q2_Write.h
+++ A4Q2_Write.h
@@ -3,17 +3,3 @@
 #include <ostream>
 #include <string>
 
-//Create a C++ header file named A4Q2_Write.h which contains function prototypes for three functions named write. 
-
-//Write the implementations for each write function in a file named A4Q2_Write.cpp. 
-//Each write function takes two arguments. The first argument is always std::ostream& os. The second arguments are an int, a float, and a std::string respectively. 
-//Each write function should stream its second argument to the passed in std::ostream. 
-//
-//Write cppunitlite unit tests that pass a std::stringstream as the first argument to each function and verify its operation. 
-//Write non unit test code that calls each write function and passes std::cout as the first argument (the cout tests are written outside the unit test framework because verification can't easily be automated). 
-//
-//Here's the prototype for the first write overload:
-//void write(std::ostream& os, int value);
-//
-//Notice that both std::stringstream and std::cout may be passed as the first argument. Both inherit from std::ostream and thus may be used where ever a std::ostream& is used. 
-//This is our first use of inheritance in C++. We'll do much more with inheritance as the course progresses.
--- A4Q3_LambdaTest.cpp
+++ A4Q3_LambdaTest.cpp
@@ -2,13 +2,13 @@
 
 #include <algorithm>
 
-//Write a lambda function that makes the following TEST pass:
+//Write a lambda function that makes the following test pass:
 //TEST(lambdaTestProblem, lambdas)
 //{
 //    const auto values = { 2, 4, 6, 8, 10, 12 };
-//	auto sum = 0;
+//	  auto sum = 0;
 //
-//	std::for_each(values.begin(), values.end(), /*define lambda function here*/);
+//	  std::for_each(values.begin(), values.end(), /*define lambda function here*/);
 //
-//	CHECK_EQUAL(42, sum);
+//	  CHECK_EQUAL(42, sum);
 //}
